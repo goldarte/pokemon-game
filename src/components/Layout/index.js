095fc1dcc8d6ccc32369dc34306a03c38dfbc0ba
Arthur Golubtsov <goldartt@gmail.com>
2021-03-14 12:28:37 +0300

    Layout: Handle empty values in style

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,19 +1,32 @@
 import s from './style.module.css';
 
 const Layout = ({title, descr, urlBg, colorBg}) => {
+    const styleRoot = {
+        backgroundImage: `${ urlBg ? `url(${urlBg}` : `none`}`,
+        backgroundColor: `${ colorBg ? colorBg : `red`}`
+    }
     return (
-        <section className={s.root} style={{
-            backgroundImage: `url(${urlBg})`,
-            backgroundColor: `${colorBg}`
-        }}>
+        // <section className={s.root} style={{
+        //     backgroundImage: `url(${urlBg})`,
+        //     backgroundColor: `${colorBg}`
+        // }}>
+        <section className={s.root} style={styleRoot}>
             <div className={s.wrapper}>
                 <article>
                     <div className={s.title}>
-                        <h3>{title}</h3>
-                        <span className={s.separator}></span>
+                    {
+                        title && (
+                            <>
+                            <h3>{title}</h3>
+                            <span className={s.separator}></span>
+                            </>
+                        )
+                    }
                     </div>
                     <div className={s.desc.full}>
-                        <p>{descr}</p>
+                    {
+                        descr && (<p>{descr}</p>)
+                    }
                     </div>
                 </article>
             </div>
