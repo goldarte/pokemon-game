2c353eca0ce074958a5f0a3765982516a6f946dd
Arthur Golubtsov <goldartt@gmail.com>
2021-03-14 11:46:46 +0300

    Header: Add defaults for arguments

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import s from './style.module.css';
 
-const Header = ({title, descr}) => {
+const Header = ({title="Default title", descr="Default description"}) => {
     return (
         <header className={s.root}>
             <div className={s.forest}></div>
