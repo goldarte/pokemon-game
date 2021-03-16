eb99a3af76eee3a32fd483251a1cfc5648c513a0
Arthur Golubtsov <goldartt@gmail.com>
2021-03-16 22:12:53 +0300

    Header: Add click handler

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,19 @@
 import s from './style.module.css';
 
-const Header = ({title="Default title", children}) => {
+const Header = ({title="Default title", children, onClickButton}) => {
+    const handleClick = () => {
+        console.log('####: HEADER');
+        onClickButton && onClickButton('game');
+    }
     return (
         <header className={s.root}>
             <div className={s.forest}></div>
             <div className={s.container}>
                 <h1>{title}</h1>
                 {children}
+                <p><button onClick={handleClick}>
+                    Start Game
+                </button></p>
             </div>
         </header>
     )
